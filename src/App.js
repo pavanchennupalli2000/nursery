7a998469ff49d55ae944d65365f4f262e37f1a61
pavanchennupalli2000 <chennupallipavanvsk@gmail.com>
2025-01-19 20:17:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/nursery" element={<LandingPage />} />
         <Route
           path="/product"
           element={<ProductPage cartItems={cartItems} setCartItems={setCartItems} />}
